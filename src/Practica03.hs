aba9dd19961b94f26dc74a383410173ec01fe32b
spelayof21 <spelayof@ciencias.unam.mx>
2026-03-28 19:42:39 -0600

    el unico test que no puedo pasar es el ultimo

--- src/Practica03.hs
+++ src/Practica03.hs
@@ -1,5 +1,6 @@
 module Practica03 where
 
+
 --Sintaxis de la logica proposicional
 data Prop = Var String | Cons Bool | Not Prop
             | And Prop Prop | Or Prop Prop
@@ -49,13 +50,24 @@ fnn (Syss p q) = And (Or (fnn (Not p)) (fnn q)) (Or (fnn (Not q)) (fnn p))
 
 --Ejercicio 2
 fnc :: Prop -> Prop
+fnc (Not (Var p)) = Not (Var p)
+fnc (Not (Cons a)) = Cons (not a)
 fnc (Cons a) = Cons a
 fnc (Var p) = Var p
-fnc (Not (Var p)) = Not (Var p)
-fnc (Not p) = Not p
-fnc (Not (Not p)) = fnc p
+fnc (Not p) = fnc (fnn (Not p))
 fnc (And p q) = And (fnc p) (fnc q)
-fnc (Not (And p q)) = Or (fnc p) (fnc q)
+fnc (Or p q) = distributividad (fnc p) (fnc q)
+fnc (Impl p q) = fnc (fnn (Impl p q))
+fnc (Syss p q) = fnc (fnn (Syss p q))
+
+
+--Función auxiliar que nos ayuda a la distributividad
+distributividad :: Prop -> Prop -> Prop
+distributividad p (And q r) = And (distributividad p q) (distributividad p r)
+distributividad (And p q) r = And (distributividad p r) (distributividad q r)
+distributividad p q = Or p q
+
+
 
 {-
 RESOLUCION BINARIA
@@ -67,11 +79,46 @@ type Clausula = [Literal]
 
 --Ejercicio 1
 clausulas :: Prop -> [Clausula]
-clausulas = undefined
+clausulas (And p q) = clausulas p ++ clausulas q
+clausulas (Or p q) =  [literales (Or p q)]
+clausulas p = [literales p]
+
+--Función auxiliar que saca los literales
+literales :: Prop -> Clausula
+literales (Or p q) = sinDuplicados (literales p ++ literales q)
+literales p = [p]
+
 
 --Ejercicio 2
 resolucion :: Clausula -> Clausula -> Clausula
-resolucion = undefined
+resolucion cl1 cl2 =
+  case compl cl1 cl2 of
+    Nothing     -> sinDuplicados (cl1 ++ cl2)
+    Just (l, m) -> sinDuplicados (eliminaUno l cl1 ++ eliminaUno m cl2)
+
+
+eliminaUno :: Eq a => a -> [a] -> [a]
+eliminaUno _ [] = []
+eliminaUno x (y:ys)
+  | x == y    = ys
+  | otherwise = y : eliminaUno x ys
+
+--Funcion auxiliar que busca el primer par
+compl :: Clausula -> Clausula -> Maybe (Literal, Literal)
+compl cl1 cl2 =
+  let pares = [(l, complemento l) | l <- cl1, complemento l `elem` cl2]
+      puros = [(l, m) | (l, m) <- pares, complemento l `notElem` cl1]
+  in case puros of
+       (x : _) -> Just x
+       []      -> case pares of
+         []      -> Nothing
+         (x : _) -> Just x
+
+
+
+sinDuplicados :: Eq a => [a] -> [a]
+sinDuplicados [] = []
+sinDuplicados (x:xs) = x : sinDuplicados (filter (/= x) xs)
 
 {-
 ALGORITMO DE SATURACION
@@ -79,9 +126,29 @@ ALGORITMO DE SATURACION
 
 --Ejercicio 1
 hayResolvente :: Clausula -> Clausula -> Bool
-hayResolvente = undefined
+hayResolvente cl1 cl2 =
+  any (\l -> complemento l `elem` cl2) cl1
+
+--Funcion para el complemento de una literal
+complemento :: Literal -> Literal
+complemento (Not p) = p
+complemento p = Not p
 
 --Ejercicio 2
 --Funcion principal que pasa la formula proposicional a fnc e invoca a res con las clausulas de la formula.
 saturacion :: Prop -> Bool
-saturacion = undefined
+saturacion p =
+  let cls = filter (not . esTautologia) (clausulas (fnc (Not p)))
+  in if null cls then False else saturar cls
+
+saturar :: [Clausula] -> Bool
+saturar cl
+  | [] `elem` cl = True
+  | null nuevas  = False
+  | otherwise    = saturar (cl ++ nuevas)
+  where
+    posibles = [ resolucion cl1 cl2 | cl1 <- cl, cl2 <- cl, hayResolvente cl1 cl2 ]
+    nuevas   = filter (`notElem` cl) posibles
+
+esTautologia :: Clausula -> Bool
+esTautologia cl = any (\l -> complemento l `elem` cl) cl
